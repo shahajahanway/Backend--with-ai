7023cc129694091a6ca1b48a8f72987de6f22135
Shahajahanway <Shahajahanway@gmail.com>
2026-01-27 21:56:38 +0530

    Connected to Database

--- Day 6 /server.js
+++ Day 6 /server.js
@@ -4,7 +4,7 @@ const app = require('./src/app')
 const mongoose = require('mongoose')
 
 function connectToDB (){
-    mongoose.connect("mongodb+srv://shaha:8sYQEEvaaaIYn6DP@cluster0.jomw5zp.mongodb.net/day6")
+    mongoose.connect("mongodb+srv://shaha:Password@cluster0.jomw5zp.mongodb.net/day6")
     .then(()=>{
         console.log("Connected to Database");
         
